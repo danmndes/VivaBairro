39036f9f60b69fa78abc073df974c891794e510f
Daniel Mendes <danmndes@gmail.com>
2024-05-30 07:42:19 -0300

    add products

--- models.py
+++ models.py
@@ -7,10 +7,16 @@ class Profile(db.Model, UserMixin):
     __tablename__ = 'profiles'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
+    email = db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     bairro = db.Column(db.String(120), nullable=False)
     tipo = db.Column(db.String, nullable=False)
+    
+    __mapper_args__ = {
+        'polymorphic_identity': 'profile',
+        'polymorphic_on': tipo
+    }
+
     # Implementing UserMixin methods
     def is_active(self):
         return True
@@ -20,18 +26,12 @@ class Profile(db.Model, UserMixin):
 
     def is_anonymous(self):
         return False
-    __mapper_args__ = {
-        'polymorphic_identity': 'profile',
-        'polymorphic_on': tipo
-    }
 
 class User(Profile):
     __tablename__ = 'users'
     id = db.Column(db.Integer, db.ForeignKey('profiles.id'), primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)  # Include username explicitly
     shop_name = None
     atuacao = None
-
     __mapper_args__ = {
         'polymorphic_identity': 'user',
     }
@@ -39,21 +39,33 @@ class User(Profile):
 class Merchant(Profile):
     __tablename__ = 'merchants'
     id = db.Column(db.Integer, db.ForeignKey('profiles.id'), primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)  # Include username explicitly
     shop_name = db.Column(db.String, nullable=False)
-    atuacao = db.Column(db.String,nullable=False)
+    atuacao = db.Column(db.String, nullable=False)
+    
+    products = db.relationship('Product', backref='merchant', lazy=True)
 
     __mapper_args__ = {
         'polymorphic_identity': 'merchant',
     }
 
 class Charity(Profile):
-    __tablename__ = 'Charity'
+    __tablename__ = 'charities'
     id = db.Column(db.Integer, db.ForeignKey('profiles.id'), primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)  # Include username explicitly
     shop_name = db.Column(db.String, nullable=False)
-    atuacao = db.Column(db.String,nullable=False)
+    atuacao = db.Column(db.String, nullable=False)
 
     __mapper_args__ = {
         'polymorphic_identity': 'charity',
     }
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    discount = db.Column(db.Float, nullable=True)
+    qr_code = db.Column(db.String, nullable=True)  # Store the QR code as a string (URL or base64)
+    visible = db.Column(db.Boolean, default=False, nullable=False)
+    path = db.Column(db.String,nullable=False)
+    merchant_id = db.Column(db.Integer, db.ForeignKey('merchants.id'), nullable=False)
